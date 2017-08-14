dd7cd77797daedc3fcc43ba04dcc0092d6051069
Akhil Nair <akhil.nair@amey.co.uk>
2017-08-14 21:07:48 +0100

    Update utils.R

--- R/utils.R
+++ R/utils.R
@@ -14,3 +14,14 @@ spark_get_master = function(port = 7077) {
   spark_master = "spark://" %&% spark_master %&% ":" %&% port
   spark_master
 }
+
+#' Infix paste0
+`%&%` = function(a, b) paste0(a, b)
+
+#' Helper to cast dates to cql dates, for use
+#' in a partition filter, for example 
+#'
+#' @export
+cassandra_cast_date = function(date) {
+  "date = cast('" %&% date %&% "' as date)"
+}
